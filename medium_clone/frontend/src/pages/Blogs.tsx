737c405f0b4afd2c15fab1e1b5bb3c29a2d3e168
Prashant-3010 <prashantparashar3010@gmail.com>
2025-04-19 23:26:12 +0530

    adding uopdated files

--- medium_clone/frontend/src/pages/Blogs.tsx
+++ medium_clone/frontend/src/pages/Blogs.tsx
@@ -1,6 +1,34 @@
+import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogCard"
 
 export const Blogs = () => {
     return <div>
-        Blogs
+        <Appbar/>
+        <div className="flex justify-center ">
+            <div className=" max-w-xl">
+                <BlogCard
+                    authorName="Prashant Parashar"
+                    title="How an Ugly Single-Page Website Makes $5000 a Month with Affiliate Marketing"
+                    content="No need to create a fancy and modern website with hundreds of pages to make money online. -Making money online is the dream for man"
+                    publishedDate="19th Apr 2025" />
+                <BlogCard
+                    authorName="Prashant Parashar"
+                    title="How an Ugly Single-Page Website Makes $5000 a Month with Affiliate Marketing"
+                    content="No need to create a fancy and modern website with hundreds of pages to make money online. -Making money online is the dream for man"
+                    publishedDate="19th Apr 2025" />
+                <BlogCard
+                    authorName="Prashant Parashar"
+                    title="How an Ugly Single-Page Website Makes $5000 a Month with Affiliate Marketing"
+                    content="No need to create a fancy and modern website with hundreds of pages to make money online. -Making money online is the dream for man"
+                    publishedDate="19th Apr 2025" />
+                <BlogCard
+                    authorName="Prashant Parashar"
+                    title="How an Ugly Single-Page Website Makes $5000 a Month with Affiliate Marketing"
+                    content="No need to create a fancy and modern website with hundreds of pages to make money online. -Making money online is the dream for man"
+                    publishedDate="19th Apr 2025" />
+            </div>
+
+        </div>
     </div>
+
 }
