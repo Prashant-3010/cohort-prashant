fa7b4abacbe54f5a3be3d0127b52fdd24d311936
Prashant-3010 <prashantparashar3010@gmail.com>
2025-06-28 14:03:49 +0530

    adding week 14.2

---
+++ week-14/14.2/app/page.tsx
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+async function getUserDetails() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+	return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+
+  return (
+    <div>
+      {userData.email}
+      {userData.name}
+    </div>
+  );
+}
